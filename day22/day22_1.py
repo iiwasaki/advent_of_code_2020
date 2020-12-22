6c57d4708f54f8d52822c7a2ed974d6473450311
Ishin <ishiniwasaki@gmail.com>
2020-12-21 23:16:54 -0800

    Part 1 finished, Part 2 needs work

---
+++ day22/day22_1.py
@@ -0,0 +1,91 @@
+"""
+Play a round of Combat with this crab in front of you.
+"""
+
+def process_input():
+    """
+    Read input file in the format:
+    Player 1 Deck:
+    #
+    #
+    #
+
+    Player 2 Deck:
+    #
+    #
+    #
+
+    and return as two different lists, each list representing a deck.
+    """
+    player1_deck = []
+    player2_deck = []
+    with open("input.txt", "r") as input_file:
+        line = input_file.readline() # Read the player 1 part
+        for line in input_file:
+            if line == "\n":
+                break # This splits player 1 and player 2
+            line = int(line)
+            player1_deck.append(line)
+        line = input_file.readline()
+        for line in input_file:
+            line = int(line)
+            player2_deck.append(line)
+
+    return player1_deck, player2_deck
+
+def play_combat(p1_deck, p2_deck):
+    """
+    Simulates a game of Combat given two decks. First player to get all
+    of the cards in their deck wins.
+    """
+    round = 1
+    while len(p1_deck) > 0 and len(p2_deck) > 0:
+        print(f"-- Round {round} --")
+        print(f"P1's deck: {p1_deck}")
+        print(f"P2's deck: {p2_deck}")
+        p1_card = p1_deck.pop(0)
+        print(f"P1 plays: {p1_card}")
+        p2_card = p2_deck.pop(0)
+        print(f"P2 plays: {p2_card}")
+        if p1_card > p2_card:
+            # Player 1 is the winner
+            print("Player 1 wins the round!")
+            p1_deck.append(p1_card)
+            p1_deck.append(p2_card)
+        else:
+            # Player 2 is the winner
+            print("Player 2 wins the round!")
+            p2_deck.append(p2_card)
+            p2_deck.append(p1_card)
+        round += 1
+        print()
+    print("== Post-game results ==")
+    print(f"Player 1's deck: {p1_deck}")
+    print(f"Player 2's deck: {p2_deck}")
+    score = -1
+    if len(p1_deck) > 0:
+        # P1 won
+        score = calculate_score(p1_deck)
+    # Otherwise P2 wins
+    score = calculate_score(p2_deck)
+    print (f"The winner's score is: {score}")
+
+    return score
+
+def calculate_score(deck):
+    """
+    Calculates the score of the winning deck (the one that has
+    values left in it).
+    """
+    multiplier = 1
+    score = 0
+    for index in range (len(deck)-1, -1, -1):
+        score += (multiplier * deck[index])
+        multiplier += 1
+
+    return score
+
+
+if __name__ == "__main__":
+    p1_deck, p2_deck = process_input()
+    score = play_combat (p1_deck, p2_deck)
